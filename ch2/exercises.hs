6a5c58b42e4c678e20588b84d5f1dea61f8f893b
tiberiualex <tiberiu.alexander@gmail.com>
2021-10-21 00:26:10 +0300

    Finish chapter 2 exercises

--- ch2/exercises.hs
+++ ch2/exercises.hs
@@ -7,3 +7,25 @@ square :: Num a => a -> a
 square x = x * x
 
 piMultiple x = pi * x
+
+-- Parentheisazation
+-- 2 + (2 * 3) - 1
+-- (^) 10 (1 + 1)
+-- (2 ^ 2) * (4 ^ 5) + 1
+
+-- Equivalent expressions
+-- 1. yes
+-- 2. yes
+-- 3. no
+-- 4. no
+-- 5. no
+
+z = 7
+y = z + 8
+x = y ^ 2
+waxOn = x * value
+        where value = 5
+
+triple x = x * 3
+
+waxOff x = triple x
