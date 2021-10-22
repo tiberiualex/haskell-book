dce44e35b8150f49413a3de2c7df36529608499a
tiberiualex <tiberiu.alexander@gmail.com>
2021-10-22 04:10:46 +0300

    Chapter 3 final exercises

--- ch3/exercises.hs
+++ ch3/exercises.hs
@@ -20,3 +20,29 @@ e1 = "hello" !! 4
 f1 = (!!) "hello" 4 --correct
 g1 = take 4 "lovely"
 h1 = take 3 "awesome" --correct
+
+--2. a-d, b-c, c-e, d-a, e-b
+
+-- Building functions
+a2 = "Curry is awesome" ++ "!"
+b2 = "Curry is awesome!" !! 4
+c2 = drop 9 "Curry is awesome!"
+
+concatenateEx :: String -> String
+concatenateEx str = str ++ "!"
+
+takeFifth :: String -> Char
+takeFifth str = str !! 4
+
+dropNine :: String -> String
+dropNine = drop 9
+
+thirdLetter :: String -> Char
+thirdLetter str = str !! 2
+
+letterIndex :: Int -> Char
+letterIndex index = "Curry is awesome!" !! index
+
+rvsr :: String -> String
+rvsr x = ((take 7) . (drop 9) $ x) ++ ((drop 5) . (take 9) $ x) ++ take 5 x
+
