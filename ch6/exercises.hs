690dc8825572899ec503471903615132092dd7f1
tiberiualex <tiberiu.alexander@gmail.com>
2021-10-29 01:23:30 +0300

    More chapter 6 exercises

--- ch6/exercises.hs
+++ ch6/exercises.hs
@@ -1,4 +1,5 @@
 import GHC.Classes (Eq)
+import Data.List
 
 -- Eq Instances
 data TisAnInteger =
@@ -64,3 +65,40 @@ instance (Eq a, Eq b) => Eq (EitherOr a b) where
 -- 3. a)
 -- 4. c)
 -- 5. a)
+
+-- Does it typecheck?
+-- 1. No, type Person doesn't derive or implement Show. It's not a Show instance
+-- 2. No, Mood doesn't derive or implement Eq. It's not an Eq instance
+-- 3. a) Mood values
+--    b) Type error. settleDown is restricted to Mood values
+--    c) It wouldn't work, Mood doesn't have an instance of Ord
+-- 4. It does typecheck.
+type Subject = String
+type Verb = String
+type Object = String
+data Sentence = Sentence Subject Verb Object deriving (Eq, Show)
+s1 = Sentence "dogs" "drool"
+s2 = Sentence "Julie" "loves" "dogs"
+
+data Rocks =
+    Rocks String deriving (Eq, Show)
+data Yeah =
+    Yeah Bool deriving (Eq, Show)
+data Papu =
+    Papu Rocks Yeah deriving (Eq, Show)
+
+-- Given a datatype declaration, what can we do?
+-- 2 and 3 typecheck.
+-- 1 doesn't because Papu data constructor is called with String and Bool, not Rocks and Yeah
+-- 4 doesn't typecheck because Rocks doesn't have an instance of Eq
+
+-- Match the types
+-- 2. No
+-- 3. Yes
+-- 4. Yes
+-- 5. Yes
+-- 6. Yes
+-- 7. No
+-- 8. No
+-- 9. Yes
+-- 10. Yes
