e55a8bbe34071214bba8dda67648e3f86d5dca9e
tiberiualex <tiberiu.alexander@gmail.com>
2021-10-30 01:11:02 +0300

    More chapter 7 exercises

--- ch7/exercises.hs
+++ ch7/exercises.hs
@@ -22,3 +22,28 @@ mflip' f x y = f y x
 
 f2 :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
 f2 (a, b, c) (d, e, f) = ((a, d), (c, f))
+
+-- Case practice
+functionC :: Ord p => p -> p -> p
+functionC x y = if (x > y) then x else y
+
+functionC' :: Ord p => p -> p -> p
+functionC' x y = case x > y of
+                    True -> x
+                    False -> y
+
+ifEvenAdd2 :: Integral p => p -> p
+ifEvenAdd2 n = if even n then (n+2) else n
+
+ifEvenAdd2' :: Integral p => p -> p
+ifEvenAdd2' n = case even n of
+                    True -> n + 2
+                    False -> n
+
+nums :: (Ord a, Num a, Num p) => a -> p
+nums x =
+    case compare x 0 of
+        LT -> -1
+        GT -> 1
+        EQ -> 0
+
