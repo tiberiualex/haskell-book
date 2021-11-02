cd215696d2f3c2e3bd11af3abfdb646d9b900f41
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-02 20:42:21 +0200

    Chapter 9 exercises

--- ch9/exercises.hs
+++ ch9/exercises.hs
@@ -20,3 +20,36 @@ eftInt = eftGeneric
 
 eftChar :: Char -> Char -> [Char]
 eftChar = eftGeneric
+
+-- Thy Fearful Symmetry
+--1.
+
+myWords :: [Char] -> [[Char]]
+myWords "" = []
+myWords (' ':xs) = myWords xs
+myWords str =
+    takeWhile noSpace str : myWords (dropWhile noSpace str)
+        where noSpace = (' ' /=)
+
+-- 2.
+firstSen = "Tyger Tyger, burning bright\n"
+secondSen = "In the forests of the night\n"
+thirdSen = "What immortal hand or eye\n"
+fourthSen = "Could frame thy fearful symmetry?"
+sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
+
+myLines :: String -> [String]
+myLines "" = []
+myLines ('\n':xs) = myLines xs
+myLines str =
+    takeWhile noNewLine str : myLines (dropWhile noNewLine str)
+        where noNewLine = ('\n' /=)
+
+-- 3.
+separateBy :: Char -> [Char] -> [[Char]]
+separateBy _ "" = []
+separateBy c ax@(x:xs)
+    | c == x = separateBy c xs
+    | otherwise =
+            takeWhile noChar ax : separateBy c (dropWhile noChar ax)
+                where noChar = (c /=)
