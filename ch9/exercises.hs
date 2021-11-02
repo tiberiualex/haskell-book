089697e1369026ab92493b0ae82aff53f925a22d
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-02 23:33:34 +0200

    Chapter 9 exercises

--- ch9/exercises.hs
+++ ch9/exercises.hs
@@ -53,3 +53,39 @@ separateBy c ax@(x:xs)
     | otherwise =
             takeWhile noChar ax : separateBy c (dropWhile noChar ax)
                 where noChar = (c /=)
+
+-- Square cubes
+mySqr = [x^2 | x <- [1..5]]
+myCube = [y^3 | y <- [1..5]]
+tuples = [(x, y) | x <- mySqr, y <- myCube]
+tuples2 = [(x, y) | x <- mySqr, x < 50, y <- myCube, y < 50]
+
+-- Will it blow up?
+-- 1. Yes
+-- 2. No
+-- 3. Yes
+-- 4. No
+-- 5. Yes
+-- 6. No
+-- 7. Yes
+-- 8 . No
+-- 9. No
+-- 10. Yes
+
+-- Is it normal form?
+-- 1. NF
+-- 2. WHNF
+-- 3. Neither
+-- 4. Neither
+-- 5. Neither?
+-- 6. WHNF
+-- 7. WHNF
+
+-- More bottom exercises
+-- 1. Error
+-- 2. Will return value
+-- 3. Error
+-- 4. It takes a string and returns a list showing which letters are vowels and which aren't
+-- 5. a) [1, 4, 9 ..] squares of the numbers 1 to 10
+--    b) [1, 10, 20]
+--    c) [15, 15, 15]
