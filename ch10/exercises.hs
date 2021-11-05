868062247ea8c9d302e582f313dfdc2a2aacca07
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-05 20:18:22 +0200

    Chapter 10 exercises

--- ch10/exercises.hs
+++ ch10/exercises.hs
@@ -17,3 +17,45 @@
 --    e) foldl (flip ((++) . show)) "" [1..5]
 --    f) foldr (flip const) 'a' [1..5]
 
+-- Rewriting functions using folds
+myAnd :: [Bool] -> Bool
+myAnd = foldr (&&) True
+
+-- 1.
+myOr :: [Bool] -> Bool
+myOr = foldr (||) False
+
+-- 2.
+myAny:: (a -> Bool) -> [a] -> Bool
+myAny f = foldr (\x acc -> f x || acc) False
+
+-- 3.
+myElem :: Eq a => a -> [a] -> Bool
+myElem a = foldr (\x acc -> (x == a) || acc) False
+
+-- 4.
+myReverse :: [a] -> [a]
+myReverse = foldr (\x acc -> acc ++ [x]) []
+
+-- 5.
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr (\x acc -> f x : acc) []
+
+-- 6.
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f = foldr (\x acc -> if f x then x : acc else acc) []
+
+-- 7.
+
+squish :: [[a]] -> [a]
+squish = foldr (++) []
+
+-- 8.
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr (\x acc -> f x ++ acc) []
+
+-- 9.
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+
