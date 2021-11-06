5dcfe19a45789ebb86f11af56922a57d7ca1154f
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-06 16:41:19 +0200

    Chapter 11 exercises

--- ch11/exercises.hs
+++ ch11/exercises.hs
@@ -70,12 +70,67 @@ data Example = MakeExample deriving Show
 -- 3. MakeExample2 :: Int -> Example2
 data Example2 = MakeExample2 Int deriving Show
 
+
+-- Exercises: Logic goats
 class TooMany a where
     tooMany :: a -> Bool
 
 instance TooMany Int where
     tooMany n = n > 42
 
+instance TooMany String where
+    tooMany s = length s > 42
+
+instance TooMany (Int, String) where
+    tooMany (i, s) = length s > 42 && i > 42
+
 newtype Goats =
     Goats Int deriving (Eq, Show, TooMany)
+
+newtype TooManyIdk = TooManyIdk (Int, String) deriving (Eq, Show, TooMany)
+
+newtype GoatFields =
+    GoatFields (Int, Int) deriving (Eq, Show)
+
+instance TooMany GoatFields where
+    tooMany (GoatFields (x, y)) = x + y > 42
+
+-- Exercises: Bool
+-- 1. The cardinality is 4
+-- 2. The cardinality is 256 (the cardinality of Int8) + 2 (the cardinality of Bool) = 258
+
+-- Constructing and deconstructing values
+
+data GuessWhat =
+    Chickenbutt deriving (Eq, Show)
+
+data Id a =
+    MkId a deriving (Eq, Show)
+
+data Product a b =
+    Product a b deriving (Eq, Show)
+
+data Sum a b =
+      First a
+    | Second b
+    deriving (Eq, Show)
+
+data RecordProduct a b =
+    RecordProduct { pfirst :: a
+                  , psecond :: b }
+                  deriving (Eq, Show)
+
+newtype NumCow =
+    NumCow Int
+    deriving (Eq, Show)
+
+newtype NumPig =
+    NumPig Int
+    deriving (Eq, Show)
+
+data Farmhouse =
+    Farmhouse NumCow NumPig
+    deriving (Eq, Show)
+
+type Farmhouse' = Product NumCow NumPig
 
