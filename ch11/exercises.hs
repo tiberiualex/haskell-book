b6e982ded8467dd65e04a57aadfb5f42e3cfbe63
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-06 17:30:55 +0200

    Chapter 11 exercises

--- ch11/exercises.hs
+++ ch11/exercises.hs
@@ -134,3 +134,46 @@ data Farmhouse =
 
 type Farmhouse' = Product NumCow NumPig
 
+-- Programmers (constructing values)
+data OperatingSystem =
+      GnuPlusLinux
+    | OpenBSDPlusNevermindJustBSDStill
+    | Mac
+    | Windows
+    deriving (Eq, Show)
+
+data ProgLang =
+      Haskell
+    | Agda
+    | Idris
+    | PureScript
+    deriving (Eq, Show)
+
+data Programmer =
+    Programmer { os :: OperatingSystem
+                , lang :: ProgLang }
+    deriving (Eq, Show)
+
+nineToFive :: Programmer
+nineToFive = Programmer { os = Mac
+                        , lang = Haskell }
+
+feelingWizardly :: Programmer
+feelingWizardly =
+    Programmer { lang = Agda
+               , os = GnuPlusLinux }
+
+allLanguages :: [ProgLang]
+allLanguages =
+    [Haskell, Agda, Idris, PureScript]
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+    [ GnuPlusLinux
+    , OpenBSDPlusNevermindJustBSDStill
+    , Mac
+    , Windows
+    ]
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer { lang = a, os = b} | a <- allLanguages, b <- allOperatingSystems ]
