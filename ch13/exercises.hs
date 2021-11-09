d7d965986a72d5e809c35d45333cce6e37b716f7
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-09 18:54:33 +0200

    Chapter 13 exercises

---
+++ ch13/exercises.hs
@@ -0,0 +1,7 @@
+-- Intermission
+-- 1. forever, when
+-- 2. Data.Bits, Database.Blacktip.Types
+-- 3. Type definitions
+-- 4. a) Control.Concurrent.MVar, Filesystem.Path.CurrentOS, Control.Concurrent
+--    b) Filesystem
+--    c) Control.Monad
