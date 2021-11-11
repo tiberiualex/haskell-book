a5e6e5d6a69a853fa4fd21e50e33dbcf8e801017
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-11 21:28:10 +0200

    Chapter 14 - QuickCheck examples

--- ch14/addition/Addition.hs
+++ ch14/addition/Addition.hs
@@ -1,9 +1,76 @@
 module Addition where
 
 import Test.Hspec
+import Test.QuickCheck
+
+main1 :: IO ()
+main1 = hspec $ do
+    describe "Addition" $ do
+        it "1 + 1 is greater than 1" $ do
+            (1 + 1) > 1 `shouldBe` True
+        it "2 + 2 is equal to 4" $ do
+            2 + 2 `shouldBe` 4
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+    where go n   d count
+           | n < d = (count, n)
+           | otherwise =
+               go (n - d) d (count + 1)
 
 main :: IO ()
 main = hspec $ do
     describe "Addition" $ do
-        it "1 + 1 is greater than 1" $ do
-            (1 + 1) > 1 `shouldBe` True
+        it "15 divided by 3 is 5" $ do
+            dividedBy 15 3 `shouldBe` (5, 0)
+        it "22 divided by 5 is 4 remainder 2" $ do
+            dividedBy 22 5 `shouldBe` (4, 2)
+        it "x + 1 is always greater than x" $ do
+            property $ \x -> x + 1 > (x :: Int)
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+    a <- arbitrary
+    b <- arbitrary
+    return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b,
+                Arbitrary c)
+            => Gen (a, b, c)
+genThreeple = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b)
+          => Gen (Either a b)
+genEither = do
+    a <- arbitrary
+    b <- arbitrary
+    elements [Left a, Right b]
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+    a <- arbitrary
+    elements [Nothing, Just a]
+
+-- frequency :: [(Int, Gen a)] -> Gen a
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
