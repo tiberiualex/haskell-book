acaa10078893d7ff2fb7a8aaa8c8e1bcc0819150
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-28 23:11:31 +0200

    Chapter 21 exercises

--- ch21/exercises.hs
+++ ch21/exercises.hs
@@ -1,3 +1,8 @@
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+import System.Directory.Internal.Prelude (Applicative)
+
 -- class (Functor t, Foldable t) => Traversable t where
 --     traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
 --     traverse f = sequenceA . fmap f
@@ -22,3 +27,15 @@
 -- Prelude> traverse Just [1, 2, 3]
 -- Just [1,2,3]
 -- fmap just over [1, 2, 3], resulting in [Just 1, Just 2, Just 3], then flip the structures around
+
+-- stack ghci --package checkers QuickCheck
+
+newtype Identity a = Identity a
+    deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+    fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+    pure = Identity
+    (Identity f) <*> (Identity a) = Identity (f a)
